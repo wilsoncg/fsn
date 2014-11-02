fb447cd429702698e42914ead514b9bcaf7a736a
wilsoncg <craig@wilsoncg.net>
2014-11-02 23:22:26 +0000

    first cut

---
+++ _references.js
@@ -0,0 +1,4 @@
+/// <reference path="packages/jquery.2.1.1/content/scripts/jquery-2.1.1.min.js" />
+/// <reference path="packages/RxJS-Main.2.3.14/content/scripts/rx.min.js" />
+/// <reference path="three.69.js" />
+/// <reference path="dat-gui.0.5.js" />
